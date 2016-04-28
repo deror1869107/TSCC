8024325aaee0837de298a90b94e4e53b9649ec6d
Yen Ching-Hsuan <yench@cs.nctu.edu.tw>
2016-04-28 10:21:50 +0000

    fixup error code in external network settings

--- os_install/chrooted.sh
+++ os_install/chrooted.sh
@@ -26,10 +26,11 @@ locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 # netctl
-echo "Description='A basic static ethernet connection'
 echo "Does this machine have external network? (Y/N)"
 read extnet
 if [ extnet == "Y" -o extnet == "y" ] ; then
+
+echo "Description='A basic static ethernet connection'
 Interface=$IF
 Connection=ethernet
 IP=static
